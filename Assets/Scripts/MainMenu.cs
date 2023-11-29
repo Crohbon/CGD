1c2ebd336b9b2db94fe5b43fc22a6a5ca0bb74d2
Fthiele2 <fthiele2@hs-mittweida.de>
2023-11-29 01:27:42 +0100

    Main Menu with basic functions

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Info()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Return()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
