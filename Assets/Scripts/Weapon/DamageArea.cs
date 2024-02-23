ac474d104b33c7a3c481f26e9f0aa06bd8a42ecf
Carl Hunger <chunger2@hs-mittweida.de>
2024-02-23 20:43:10 +0100

    weapon collision adjusted, added damageArea to log

--- Assets/Scripts/Weapon/DamageArea.cs
+++ Assets/Scripts/Weapon/DamageArea.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class DamageArea : MonoBehaviour {
     [SerializeField] private float _damage;
+    [SerializeField] private string _areaName;
     
     private void OnTriggerEnter2D(Collider2D other) {
+        EntityHealth hitEntityHealth = other.GetComponent<EntityHealth>();
         if (other.CompareTag("Player")){
-            GameEvents.Instance.OnPlayerHit(other.GetComponent<EntityHealth>(), _damage);
+            GameEvents.Instance.OnPlayerHit(hitEntityHealth, _damage);
+            Logger.LogWeaponImpact(hitEntityHealth.PlayerIndex, _areaName, _damage, hitEntityHealth.PlayerIndex, (hitEntityHealth.CurrentHealth - _damage) <= 0);
         }
     }
 }
